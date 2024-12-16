db0b386d06ac640f37851ed86260baf2f3b58206
Arpiit K <kesariarpit5@gmail.com>
2024-12-16 20:31:48 +0530

    first commit

---
+++ app.js
@@ -0,0 +1,10 @@
+const heading = React.createElement("h1", {}, "HELLish from REACT");
+
+const rare2 = ReactDOM.createRoot(document.getElementById("rare2"));
+rare2.render(heading);
+
+const headingt = React.createElement("h1", {}, "HELL2 from REACT");
+
+const raree = ReactDOM.createRoot(document.getElementById("rare3"));
+raree.render(headingt);
+console.log(heading);
